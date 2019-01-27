d47f0cf0e414e9e11e29f0edab177701cc79bc6c
SheekhaJ <sheekhaj4893@gmail.com>
2019-01-26 20:47:55 -0800

    Added query to get listings.

--- src/com/osucm/common/constants/SqlConstants.java
+++ src/com/osucm/common/constants/SqlConstants.java
@@ -3,5 +3,6 @@ package com.osucm.common.constants;
 public interface SqlConstants {
 	
 	String INSERT_NOTIFY_EMAIL = "INSERT INTO notify_list (email) VALUES (?) ON DUPLICATE KEY UPDATE email = ?";
+	String VIEW_LISTINGS = "SELECT * FROM LISTINGS WHERE TYPE=?";
 
 }
